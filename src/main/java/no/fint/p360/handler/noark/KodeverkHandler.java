74a63fad22e510f1d244af4e7a3650ae78c86614
Asgeir Nilsen <asgeir@fintlabs.no>
2020-09-28 12:20:45 +0200

    Export Noark common coding system values.

--- src/main/java/no/fint/p360/handler/noark/KodeverkHandler.java
+++ src/main/java/no/fint/p360/handler/noark/KodeverkHandler.java
@@ -1,6 +1,7 @@
 package no.fint.p360.handler.noark;
 
 import lombok.extern.slf4j.Slf4j;
+import no.fint.arkiv.NoarkMetadataService;
 import no.fint.event.model.Event;
 import no.fint.event.model.ResponseStatus;
 import no.fint.event.model.Status;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static no.fint.model.administrasjon.arkiv.ArkivActions.*;
 
@@ -29,21 +31,24 @@ public class KodeverkHandler implements Handler {
 
     @PostConstruct
     public void init() {
-        suppliers.put(GET_ALL_DOKUMENTSTATUS, kodeverkRepository::getDokumentStatus);
-        suppliers.put(GET_ALL_DOKUMENTTYPE, kodeverkRepository::getDokumentType);
-        suppliers.put(GET_ALL_JOURNALPOSTTYPE, kodeverkRepository::getJournalpostType);
-        suppliers.put(GET_ALL_JOURNALSTATUS, kodeverkRepository::getJournalStatus);
-        suppliers.put(GET_ALL_KORRESPONDANSEPARTTYPE, kodeverkRepository::getKorrespondansepartType);
+        suppliers.put(GET_ALL_DOKUMENTSTATUS, merge(noarkMetadataService::getDokumentStatus, kodeverkRepository::getDokumentStatus));
+        suppliers.put(GET_ALL_DOKUMENTTYPE, merge(noarkMetadataService::getDokumentType, kodeverkRepository::getDokumentType));
+        suppliers.put(GET_ALL_JOURNALPOSTTYPE, merge(noarkMetadataService::getJournalpostType, kodeverkRepository::getJournalpostType));
+        suppliers.put(GET_ALL_JOURNALSTATUS, merge(noarkMetadataService::getJournalStatus, kodeverkRepository::getJournalStatus));
+        suppliers.put(GET_ALL_KORRESPONDANSEPARTTYPE, merge(noarkMetadataService::getKorrespondansepartType, kodeverkRepository::getKorrespondansepartType));
         suppliers.put(GET_ALL_MERKNADSTYPE, kodeverkRepository::getMerknadstype);
-        suppliers.put(GET_ALL_PARTROLLE, kodeverkRepository::getPartRolle);
-        suppliers.put(GET_ALL_SAKSSTATUS, kodeverkRepository::getSaksstatus);
+        suppliers.put(GET_ALL_PARTROLLE, merge(noarkMetadataService::getPartRolle, kodeverkRepository::getPartRolle));
+        suppliers.put(GET_ALL_SAKSSTATUS, merge(noarkMetadataService::getSaksStatus, kodeverkRepository::getSaksstatus));
         suppliers.put(GET_ALL_SKJERMINGSHJEMMEL, kodeverkRepository::getSkjermingshjemmel);
         suppliers.put(GET_ALL_TILGANGSRESTRIKSJON, kodeverkRepository::getTilgangsrestriksjon);
-        suppliers.put(GET_ALL_TILKNYTTETREGISTRERINGSOM, kodeverkRepository::getTilknyttetRegistreringSom);
-        suppliers.put(GET_ALL_VARIANTFORMAT, kodeverkRepository::getVariantformat);
+        suppliers.put(GET_ALL_TILKNYTTETREGISTRERINGSOM, merge(noarkMetadataService::getTilknyttetRegistreringSom, kodeverkRepository::getTilknyttetRegistreringSom));
+        suppliers.put(GET_ALL_VARIANTFORMAT, merge(noarkMetadataService::getVariantformat, kodeverkRepository::getVariantformat));
         suppliers.put(GET_ALL_KLASSIFIKASJONSSYSTEM, kodeverkRepository::getKlassifikasjonssystem);
         suppliers.put(GET_ALL_KLASSE, kodeverkRepository::getKlasse);
     }
+
+    @Autowired
+    private NoarkMetadataService noarkMetadataService;
 
     @Autowired
     private KodeverkRepository kodeverkRepository;
@@ -71,4 +76,7 @@ public class KodeverkHandler implements Handler {
         return kodeverkRepository.health();
     }
 
+    private Supplier<List<? extends FintLinks>> merge(Supplier<Stream<? extends FintLinks>> common, Supplier<List<? extends FintLinks>> local) {
+        return () -> Stream.concat(common.get(), local.get().stream()).collect(Collectors.toList());
+    }
 }
