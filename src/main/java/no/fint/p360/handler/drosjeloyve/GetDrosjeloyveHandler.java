4587dddfe82ad71f298cc74412a75fa5cfbca6e1
Frode Sjovatsen <frode@fintlabs.no>
2020-10-18 15:25:03 +0200

    AD-50 AD-17 #comment Added handlers and data services for drosjeløyve

---
+++ src/main/java/no/fint/p360/handler/drosjeloyve/GetDrosjeloyveHandler.java
@@ -0,0 +1,19 @@
+package no.fint.p360.handler.drosjeloyve;
+
+import no.fint.event.model.Event;
+import no.fint.model.resource.FintLinks;
+import no.fint.p360.handler.Handler;
+
+import java.util.Set;
+
+public class GetDrosjeloyveHandler implements Handler {
+    @Override
+    public void accept(Event<FintLinks> fintLinksEvent) {
+
+    }
+
+    @Override
+    public Set<String> actions() {
+        return null;
+    }
+}
