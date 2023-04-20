bc1f4ee05b4bf4498d85edafeaaa2724f291105b
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2023-04-20 11:47:34 +0200

    §8.1 - DispensasjonAutomatiskFredaKulturminne (#81)
    * ARKIV-3 GetDispensasjonAutomatiskFredaKulturminne
    
    * ARKIV-3 UpdateDispensasjonAutomatiskFredaKulturminne
    
    * ARKIV-3 Upgraded case-default
    
    * ARKIV-3 Upgraded case-default to latest
    
    * ARKIV-3 Kosmetiske endringer
    
    * ARKIV-3 Upgraded dependencies

---
+++ src/main/java/no/fint/p360/data/kulturminne/DispensasjonAutomatiskFredaKulturminneService.java
@@ -0,0 +1,34 @@
+package no.fint.p360.data.kulturminne;
+
+import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.fint.model.resource.FintLinks;
+import no.fint.model.resource.arkiv.kulturminnevern.DispensasjonAutomatiskFredaKulturminneResource;
+import no.fint.p360.model.FilterSet;
+import no.fint.p360.service.CaseQueryService;
+import no.fint.p360.service.FilterSetService;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+
+@Service
+public class DispensasjonAutomatiskFredaKulturminneService {
+
+    private final DispensasjonAutomatiskFredaKulturminneFactory dispensasjonAutomatiskFredaKulturminneFactory;
+    private final CaseQueryService caseQueryService;
+    private final FilterSet filterSet;
+
+    public DispensasjonAutomatiskFredaKulturminneService(
+            DispensasjonAutomatiskFredaKulturminneFactory dispensasjonAutomatiskFredaKulturminneFactory,
+            CaseQueryService caseQueryService, FilterSetService filterSetService) {
+        this.dispensasjonAutomatiskFredaKulturminneFactory = dispensasjonAutomatiskFredaKulturminneFactory;
+        this.caseQueryService = caseQueryService;
+        filterSet = filterSetService.getFilterSetForCaseType(DispensasjonAutomatiskFredaKulturminneResource.class);
+    }
+
+    public void getDispensasjonAutomatiskFredaKulturminneForQuery(String query, Event<FintLinks> response) {
+        response.setData(new LinkedList<>());
+        caseQueryService.query(filterSet, query).map(dispensasjonAutomatiskFredaKulturminneFactory::toFintResource).forEach(response::addData);
+        response.setResponseStatus(ResponseStatus.ACCEPTED);
+    }
+}
