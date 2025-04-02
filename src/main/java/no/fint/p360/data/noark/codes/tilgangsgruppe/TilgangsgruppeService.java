12f1bab0e52f3ec4d8c130e4a5763289d36a404a
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2025-04-02 07:35:19 +0200

    No need for default tilgangsgruppe

--- src/main/java/no/fint/p360/data/noark/codes/tilgangsgruppe/TilgangsgruppeService.java
+++ src/main/java/no/fint/p360/data/noark/codes/tilgangsgruppe/TilgangsgruppeService.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.arkiv.kodeverk.TilgangsgruppeResource;
 import no.fint.p360.data.p360.AccessGroupService;
 import no.p360.model.AccessGroupService.GetAccessGroupsArgs;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Stream;
@@ -15,20 +12,20 @@ import java.util.stream.Stream;
 @Service
 public class TilgangsgruppeService {
 
-    @Value("${fint.p360.defaults.tilgangsgruppe:Alle}")
-    private String defaultTilgangsgruppe;
+    private final AccessGroupService accessGroupService;
+    private final TilgangsgruppeFactory tilgangsgruppeFactory;
 
-    @Autowired
-    private AccessGroupService accessGroupService;
-
-    @Autowired
-    private TilgangsgruppeFactory tilgangsgruppeFactory;
+    public TilgangsgruppeService(AccessGroupService accessGroupService, TilgangsgruppeFactory tilgangsgruppeFactory) {
+        this.accessGroupService = accessGroupService;
+        this.tilgangsgruppeFactory = tilgangsgruppeFactory;
+    }
 
     public Stream<TilgangsgruppeResource> getTilgangsgruppeResource() {
-        log.info("Getting tilgangsgrupper...");
+
         GetAccessGroupsArgs args = new GetAccessGroupsArgs();
         args.setIncludeMembers(false);
         args.setMaxRows(0);
+
         return accessGroupService.getAccessGroups(args)
             .stream()
             .map(tilgangsgruppeFactory::toFintResource);
