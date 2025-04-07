372f35761d81d16c2c7a3632d3c462f739c3b281
Nils-Odd Solberg <nilsodd@fintlabs.no>
2025-04-07 11:58:22 +0200

    Let's return an empty tilgangsgruppe if we run into trouble.

--- src/main/java/no/fint/p360/data/noark/codes/tilgangsgruppe/TilgangsgruppeService.java
+++ src/main/java/no/fint/p360/data/noark/codes/tilgangsgruppe/TilgangsgruppeService.java
@@ -3,9 +3,13 @@ package no.fint.p360.data.noark.codes.tilgangsgruppe;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.arkiv.kodeverk.TilgangsgruppeResource;
 import no.fint.p360.data.p360.AccessGroupService;
+import no.p360.model.AccessGroupService.AccessGroup;
 import no.p360.model.AccessGroupService.GetAccessGroupsArgs;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -26,8 +30,12 @@ public class TilgangsgruppeService {
         args.setIncludeMembers(false);
         args.setMaxRows(0);
 
-        return accessGroupService.getAccessGroups(args)
-            .stream()
-            .map(tilgangsgruppeFactory::toFintResource);
+        return Optional.ofNullable(accessGroupService.getAccessGroups(args))
+                .orElseGet((() -> {
+                    log.warn("Not able to get access groups! Do you have the right privileges?");
+                    return List.of();
+                }))
+                .stream()
+                .map(tilgangsgruppeFactory::toFintResource);
     }
 }
