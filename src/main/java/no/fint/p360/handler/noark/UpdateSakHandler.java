0afcb6784cdf1d232c21e3b92ccbaabd300a7a4a
Nils-Odd Solberg <nilsodd@fintlabs.no>
2025-05-15 15:32:27 +0200

    Remove previously temporary logging of SakResource.

--- src/main/java/no/fint/p360/handler/noark/UpdateSakHandler.java
+++ src/main/java/no/fint/p360/handler/noark/UpdateSakHandler.java
@@ -94,7 +94,6 @@ public class UpdateSakHandler implements Handler {
             }
             createCase(response, sakResource);
         } else if (operation == Operation.UPDATE) {
-            log.info("UpdateCase (SakResource): {}", sakResource);
             caseDefaultsService.applyDefaultsForUpdate(caseProperties, sakResource);
             if (!validationService.validate(response, sakResource.getJournalpost())) {
                 return;
